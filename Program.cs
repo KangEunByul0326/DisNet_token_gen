60ef5c1e40bd90bd71518c0c13d148ada5868302
KangEunByul0326 <89460943+KangEunByul0326@users.noreply.github.com>
2021-11-16 23:49:24 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+using System.Threading;
+
+class Program
+{   
+    public static async Task Main()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync();
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://discord.com/");
+        await page.ClickAsync("#app-mount > div > div > div.grid-3Ykf_K.heroBackground-3m0TRU > div.row-3wW-Fx.heroContainer-3j1eQg > div > div.ctaContainer-3vWJHU > button");
+        await Thread.Sleep(1000);
+        await page.TypeAsync("input.username-27KRPU", "SlashToken");
+        await page.WaitForSelectorAsync("iframe");
+        await Thread.Sleep(3000);
+        await page.ClickAsync("iframe")
+        
+    }
+}
